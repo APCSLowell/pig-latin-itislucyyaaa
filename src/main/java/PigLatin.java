145f3a5b8b4f5bb353c6103f7a3c62d21a3b9e9a
itislucyyaaa <142839187+itislucyyaaa@users.noreply.github.com>
2024-01-26 11:19:06 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -43,7 +43,7 @@ public class PigLatin {
       if(sWord.substring(0,1).equals("a") || sWord.substring(0,1).equals("e")|| sWord.substring(0,1).equals("i")|| sWord.substring(0,1).equals("o")|| sWord.substring(0,1).equals("u")){
         return sWord + "way";
       }else if(sWord.length()>2 && sWord.substring(0,2).equals("qu")){
-        return sWord.substring(2,sWord.length()-1) + "quay";
+        return sWord.substring(2,sWord.length()) + "quay";
       }
       if(findFirstVowel(sWord) != -1) {
         for(int i=0; i<sWord.length();i++){
